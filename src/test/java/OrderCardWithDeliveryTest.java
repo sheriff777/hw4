1b0182312836d2be743b08498986619edde84d32
sheriff777 <aibek.syd@gmail.com>
2020-02-07 22:47:27 +0600

    ten

--- src/test/java/OrderCardWithDeliveryTest.java
+++ src/test/java/OrderCardWithDeliveryTest.java
@@ -21,9 +21,7 @@ class OrderCardWithDeliveryTest {
     SelenideElement form = $("form.form");
     LocalDate nowDate =  LocalDate.now();
     LocalDate plusSevenDays = nowDate.plusDays(7);
-    String DateForDeliver =  plusSevenDays.toString();
     String DateForDelivery = plusSevenDays.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    private Condition visible;
 
 
     @BeforeEach
@@ -39,7 +37,6 @@ class OrderCardWithDeliveryTest {
 
 
     @DisplayName("Should register card for delivery")
-
     @Test
     void shouldOrderCardWithDelivery() {
         form.$("[data-test-id=city] input").setValue("Тула");
@@ -49,7 +46,6 @@ class OrderCardWithDeliveryTest {
         form.$("[data-test-id=phone] input").setValue("+72544112250");
         form.$("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        //     $("[data-test-id=notification]").waitUntil(visible, 15000);
         $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!"));
         $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + DateForDelivery));
 
