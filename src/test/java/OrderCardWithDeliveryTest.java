5a57718eacaee1f5d14b4c526dbeca40e2441a9e
sheriff777 <aibek.syd@gmail.com>
2020-02-09 15:02:23 +0600

    Final repair all mistakes

--- src/test/java/OrderCardWithDeliveryTest.java
+++ src/test/java/OrderCardWithDeliveryTest.java
@@ -1,53 +1,50 @@
-
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Condition.*;
 
 
-class OrderCardWithDeliveryTest {
+public class OrderCardWithDeliveryTest {
 
-    SelenideElement form = $("form.form");
-    LocalDate nowDate =  LocalDate.now();
-    LocalDate plusSevenDays = nowDate.plusDays(7);
-    String DateForDelivery = plusSevenDays.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    SelenideElement form = $("form");
+    SelenideElement city = form.$("input[placeholder='Город'");
+    SelenideElement dateOrder = form.$("input[placeholder='Дата встречи']");
+    SelenideElement name = form.$("input[name='name']");
+    SelenideElement phoneNumber = form.$("input[name='phone']");
+    SelenideElement checkBox = form.$(".checkbox__text");
+    SelenideElement buttonConfirm = $$(".button__icon~.button__text").find(exactText("Забронировать"));
+    SelenideElement notification = $(withText("Успешно"));
+
+    private String getOrderDate(int plusDays) {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.YYYY");
+        LocalDate currentDate = LocalDate.now();
+        LocalDate controlDate = currentDate.plusDays(plusDays);
+        String formatControlDate = controlDate.format(format);
+        return formatControlDate;
+    }
 
 
     @BeforeEach
-    void setup() {
-        open("http:\\localhost:9999");
-        Configuration.waitUntil(exist, 15000);
+    void openLocalhost() {
+        open("http://localhost:9999");
     }
 
-    @AfterEach
-    void closeBrowser() {
-        Selenide.close();
-    }
-
-
-    @DisplayName("Should register card for delivery")
     @Test
-    void shouldOrderCardWithDelivery() {
-        form.$("[data-test-id=city] input").setValue("Тула");
-        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        form.$("[data-test-id=date] .input__control").setValue(DateForDelivery);
-        form.$("[data-test-id=name] input").setValue("Сыдыгалиев Айбек");
-        form.$("[data-test-id=phone] input").setValue("+72544112250");
-        form.$("[data-test-id=agreement]").click();
-        $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification] .notification__title").shouldHave(exactText("Успешно!"));
-        $("[data-test-id=notification] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + DateForDelivery));
-
+    void shouldReservationCard() {
+        city.setValue("Тула");
+        dateOrder.doubleClick().sendKeys(Keys.BACK_SPACE);
+        dateOrder.setValue(getOrderDate(7));
+        name.setValue("Айбек Сыдыгалиев");
+        phoneNumber.setValue("+75668785550");
+        checkBox.click();
+        buttonConfirm.click();
+        notification.waitUntil(exist, 15000);
     }
 }
